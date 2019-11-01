0931d552518936bdc6a8381c6c593d4075a06e10
GermanSincovic <german.sincovic.92@gmail.com>
2019-11-01 16:08:37 +0200

    db connection

--- db/dbconfig.php
+++ db/dbconfig.php
@@ -9,36 +9,47 @@ function getDbConnect($_serv = null){
 	$dbUser = "admin";
 	$dbPass = "admin";
 
-    $con = mysql_connect($dbHost, $dbUser, $dbPass);
-    if( false  ===  $con){
-        print_r('Connection failed');
-        return false;
-    }
-    
-    mysql_query("SET NAMES 'utf8';", $con);
-    mysql_query("SET CHARACTER SET 'utf8';", $con);
-    mysql_query("set character_set_client='utf8'", $con);
-    mysql_query("set character_set_results='utf8'", $con);
-    mysql_query("set collation_connection='utf8_general_ci'", $con);
+    $mysqli = new mysqli($dbHost, $dbUser, $dbPass);
 
-    if(mysql_select_db($dbName, $con)){
-        return $con;
-    }else{
-        var_dump(mysql_query("CREATE DATABASE espresso"));
-        var_dump(mysql_select_db($dbName, $con));
-        var_dump(mysql_query("CREATE TABLE `clients` (
-                    `id` INT NOT NULL AUTO_INCREMENT,
-                    `name` VARCHAR(250) NOT NULL,
-                    `created` TIMESTAMP NOT NULL,
-                    `active` TINYINT(1) NOT NULL,
-                    `phone` VARCHAR(50) NOT NULL,
-                    `email` VARCHAR(50) NOT NULL,
-                    `owner` VARCHAR(250) NOT NULL,
-                    PRIMARY KEY (`id`) )
-                    COLLATE='utf8_general_ci';"));
+    try {
+
+        $mysqli->query("CREATE DATABASE espresso");
+
+    } finally {
+
+        $mysqli->select_db("espresso");
+        
     }
+
+
+
+    // $mysqli->query("CREATE TABLE `clients` (
+    //                  `id` INT NOT NULL AUTO_INCREMENT,
+    //                  `name` VARCHAR(250) NOT NULL,
+    //                  `created` TIMESTAMP NOT NULL,
+    //                  `active` TINYINT(1) NOT NULL,
+    //                  `phone` VARCHAR(50) NOT NULL,
+    //                  `email` VARCHAR(50) NOT NULL,
+    //                  `owner` VARCHAR(250) NOT NULL,
+    //                  PRIMARY KEY (`id`) )
+    //                  COLLATE='utf8_general_ci';");
+    // vardump($mysqli);
 
 }
 
+
+
+        // mysqli_query("CREATE DATABASE espresso");
+        // mysqli_select_db($dbName, $con);
+        // mysqli_query("CREATE TABLE `espresso.clients` (
+        //             `id` INT NOT NULL AUTO_INCREMENT,
+        //             `name` VARCHAR(250) NOT NULL,
+        //             `created` TIMESTAMP NOT NULL,
+        //             `active` TINYINT(1) NOT NULL,
+        //             `phone` VARCHAR(50) NOT NULL,
+        //             `email` VARCHAR(50) NOT NULL,
+        //             `owner` VARCHAR(250) NOT NULL,
+        //             PRIMARY KEY (`id`) )
+        //             COLLATE='utf8_general_ci';");
 
 ?>
