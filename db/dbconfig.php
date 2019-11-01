4140bce0b5a2ca75496ed62ba4c36e4824380f1f
GermanSincovic <german.sincovic.92@gmail.com>
2019-11-01 15:12:01 +0200

    login page

--- db/dbconfig.php
+++ db/dbconfig.php
@@ -5,24 +5,40 @@ define("DOMAIN", "http://espressolight.loc/");
 function getDbConnect($_serv = null){
 
 	$dbHost = "localhost";
-	$dbName = "esspresso";
+	$dbName = "espresso";
 	$dbUser = "admin";
 	$dbPass = "admin";
 
-    $conn = new mysqli($dbHost, $dbUser, $dbPass);
-    if ($conn->connect_error) {
-       die("Ошибка подключения: " . $conn->connect_error);
+    $con = mysql_connect($dbHost, $dbUser, $dbPass);
+    if( false  ===  $con){
+        print_r('Connection failed');
+        return false;
+    }
+    
+    mysql_query("SET NAMES 'utf8';", $con);
+    mysql_query("SET CHARACTER SET 'utf8';", $con);
+    mysql_query("set character_set_client='utf8'", $con);
+    mysql_query("set character_set_results='utf8'", $con);
+    mysql_query("set collation_connection='utf8_general_ci'", $con);
+
+    if(mysql_select_db($dbName, $con)){
+        return $con;
+    }else{
+        mysql_query("CREATE DATABASE espresso");
+        mysql_select_db($dbName, $con);
+        mysql_query("CREATE TABLE `clients` (
+                    `id` INT NOT NULL AUTO_INCREMENT,
+                    `name` VARCHAR(250) NOT NULL,
+                    `created` TIMESTAMP NOT NULL,
+                    `active` TINYINT(1) NOT NULL,
+                    `phone` VARCHAR(50) NOT NULL,
+                    `email` VARCHAR(50) NOT NULL,
+                    `owner` VARCHAR(250) NOT NULL,
+                    PRIMARY KEY (`id`) )
+                    COLLATE='utf8_general_ci';");
     }
 
-    if ($conn->query("CREATE DATABASE espresso") === TRUE) {
-       $conn = new mysqli($dbHost, $dbUser, $dbPass);
-        if ($conn->connect_error) {
-            die("Ошибка подключения: " . $conn->connect_error);
-        }
-    }
 }
-
-
 
 
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,28 @@
 <?
-require_once('db/dbmodels.php');
 
-getDbConnect();
+session_start();
+
+if($_POST['password']){
+	$_SESSION['password'] = md5($_POST['password']);
+}
+
+if($_SESSION['password'] == md5('admin')){
+
+	require_once('db/dbmodels.php');
+
+	getDbConnect();
+
+	//include_once('admin/v_control_panel.php');
+
+	echo 'Goooood!';
+
+} else {
+
+	include_once('admin/v_login.php');
+
+}
+
+
+
 
 ?>
