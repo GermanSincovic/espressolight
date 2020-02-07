c274d1ee1cd012632ea9066d4d9374a04412d94e
Ivan Didyk <41524878+GermanSincovic@users.noreply.github.com>
2020-02-07 12:49:57 +0200

    DataLister

--- js/internal/data_lister.js
+++ js/internal/data_lister.js
@@ -1,17 +1,17 @@
 function DataLister(data, target){
 
-	this.wrapper = document.createElement('ul');
+	this.wrapper = document.createElement('div');
 	this.wrapper.className = "list-group";
 
 	this.addLine = function(text, link){
-		var el = document.createElement('li');
-			el.className = "list-group-item";
+		// var el = document.createElement('li');
+		// 	el.className = "list-group-item";
 		var a = document.createElement('a');
 			a.className = "list-group-item list-group-item-action";
 			a.href = link;
 			a.innerText = text;
-			el.append(a);
-			this.wrapper.append(el);
+			// el.append(a);
+			this.wrapper.append(a);
 	}
 
 	if(data.length > 0){
