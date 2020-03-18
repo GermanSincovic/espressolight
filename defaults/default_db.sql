8f9cb78e5fab1209ee6f2e154befcd9977fa76aa
Ivan Didyk <41524878+GermanSincovic@users.noreply.github.com>
2020-03-18 15:17:40 +0200

    DB default structure

--- defaults/default_db.sql
+++ defaults/default_db.sql
@@ -1,84 +1,23 @@
-CREATE TABLE IF NOT EXISTS `companies` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uid` int(11) NOT NULL,
-  `name` varchar(250) NOT NULL DEFAULT '',
-  `created` datetime NOT NULL,
-  `active` varchar(10) NOT NULL DEFAULT 'no',
-  `phone` varchar(50) NOT NULL DEFAULT '',
-  `email` varchar(100) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`),
-  KEY `uid` (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+CREATE DATABASE IF NOT EXISTS `espressolight`;
 
-CREATE TABLE IF NOT EXISTS `contragents` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uid` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`),
-  KEY `uid` (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+USE `espressolight`;
 
-CREATE TABLE IF NOT EXISTS `expenses` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nid` int(11) NOT NULL,
-  `count` int(11) NOT NULL,
-  `datetime` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `accounts` (`account_id` bigint(20) NOT NULL AUTO_INCREMENT,`owner_id` bigint(20) NOT NULL,`account_name` varchar(250) NOT NULL DEFAULT '',PRIMARY KEY (`account_id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `incoming` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uid` int(11) NOT NULL,
-  `caid` int(11) NOT NULL,
-  `cid` int(11) NOT NULL,
-  `nid` int(11) NOT NULL,
-  `datetime` datetime NOT NULL,
-  `count` int(11) NOT NULL DEFAULT 0,
-  `amount` varchar(50) NOT NULL DEFAULT '',
-  `price` varchar(50) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`),
-  KEY `uid` (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `companies` (`company_id` bigint(20) NOT NULL AUTO_INCREMENT,PRIMARY KEY (`company_id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `nomenclature` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uid` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL DEFAULT '',
-  `measurement` varchar(15) NOT NULL DEFAULT '',
-  `excise` varchar(5) NOT NULL DEFAULT 'no',
-  PRIMARY KEY (`id`),
-  KEY `uid` (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `contragents` (`contragent_id` bigint(20) NOT NULL AUTO_INCREMENT,PRIMARY KEY (`contragent_id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `pricing` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uid` int(11) NOT NULL,
-  `nid` int(11) NOT NULL,
-  `price` varchar(50) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`),
-  KEY `uid` (`uid`),
-  KEY `nid` (`nid`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+CREATE TABLE IF NOT EXISTS `expenses` (`expense_id` bigint(20) NOT NULL AUTO_INCREMENT,PRIMARY KEY (`expense_id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `recipes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uid` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL DEFAULT '',
-  `components` varchar(150) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`),
-  KEY `uid` (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `incoming` (`incoming_id` bigint(20) NOT NULL AUTO_INCREMENT,PRIMARY KEY (`incoming_id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL DEFAULT '',
-  `surname` varchar(50) NOT NULL DEFAULT '',
-  `role` varchar(50) NOT NULL DEFAULT '',
-  `company` varchar(10) NOT NULL DEFAULT '',
-  `login` varchar(50) NOT NULL DEFAULT '',
-  `token` varchar(100) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `token` (`token`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `nomenclature` (`nomenclature_id` bigint(20) NOT NULL AUTO_INCREMENT,PRIMARY KEY (`nomenclature_id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `users` SET `name`='admin', `surname`='admin', `role`='master', `company`='', `login`='admin', `token`='fe613c9795fc44c247558d276a557b486c85d25b';
+CREATE TABLE IF NOT EXISTS `prices` (`price_id` bigint(20) NOT NULL AUTO_INCREMENT,PRIMARY KEY (`price_id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `recipes` (`recipe_id` bigint(20) NOT NULL AUTO_INCREMENT,PRIMARY KEY (`recipe_id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `roles` (`role_id` bigint(20) NOT NULL AUTO_INCREMENT,`role_name` varchar(250) NOT NULL DEFAULT '',PRIMARY KEY (`role_id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `users` (`user_id` bigint(20) NOT NULL AUTO_INCREMENT,`account_id` bigint(20) NOT NULL,`role_id` bigint(20) NOT NULL,`user_first_name` varchar(250) NOT NULL DEFAULT '',`user_second_name` varchar(250) NOT NULL DEFAULT '',`user_full_name` varchar(250) NOT NULL DEFAULT '',`user_email` varchar(250) NOT NULL DEFAULT '',`user_phone` varchar(250) NOT NULL DEFAULT '',`user_comment` varchar(250) NOT NULL DEFAULT '',`user_active` tinyint(4) NOT NULL DEFAULT 0,`user_create_date` timestamp NOT NULL DEFAULT current_timestamp(),PRIMARY KEY (`user_id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
