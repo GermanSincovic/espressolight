62be8aaa6bf2d680a9f9cd3bf851f2dfb6abdebb
Ivan Didyk <41524878+GermanSincovic@users.noreply.github.com>
2020-03-19 02:45:18 +0200

    Router refactoring

--- model/Router.php
+++ model/Router.php
@@ -1,35 +1,30 @@
 <?
 class Router{
 
-    public $endpointPattern = '';
+    public $APIEndpointPattern = false;
 
     private $url;
     private $varsGet;
+    private $path;
     private $varsPath;
 
     public function __construct(){
         $this -> url = $_SERVER['REQUEST_URI'];
-        $this -> parseUrl();
-    }
-
-    private function parseUrl(){
         $tmp = explode("?", $this -> url);
-        $this -> varsPath = preg_split('@/@', $tmp[0], NULL, PREG_SPLIT_NO_EMPTY);
+        $this -> path = $tmp[0];
+        $this -> varsPath = preg_split('@/@', $this -> path, NULL, PREG_SPLIT_NO_EMPTY);
         parse_str($tmp[1], $this -> varsGet);
+        if($this -> isAPI()){
+            $this -> APIEndpointPattern = $this -> getAPIEndpointPattern();
+        }
     }
 
-    public function getAPIEndpointPattern($url){
-        $e = $url;
+    public function isAPI(){
+        return ($this -> varsPath[0] == 'api' ? true : false);
+    }
 
-        if($e == "/api/v1/auth/login" || $e == "/api/v1/auth/logout"){ return $e; }
-
-        $e = explode("?", $e)[0];
-        $e = explode("/", $e);
-        if(preg_match("/\D/", $e[3]) > 0){
-            return false;
-        }
-        $ending = preg_match("/^\d+$/", $e[3]) > 0 ? "/{id}" : "" ;
-        return "/".$e[1]."/".$e[2].$ending;
+    public function getAPIEndpointPattern(){
+        return preg_replace('/(\/)(\d+)(?=(\/|$))/', '{id}', $this -> path);
     }
 
 }
