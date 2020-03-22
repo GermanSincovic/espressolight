f712a6bff5a3347130d98ae521065ac6bdc93891
Ivan Didyk <41524878+GermanSincovic@users.noreply.github.com>
2020-03-22 17:18:04 +0200

    API refactoring

--- model/API/API_Auth.php
+++ model/API/API_Auth.php
@@ -13,13 +13,13 @@ class API_Auth extends API{
         global $Parser;
         global $DB_connection;
 
-//         $Parser -> DBResponseToArray(
-         echo   $DB_connection -> query(
+         echo $Parser -> DBResponseToArray(
+            $DB_connection -> query(
                 "SELECT * FROM `users` WHERE "
                 ."`user_login`='". $this -> body['login'] ."' AND"
                 ."`user_password`='" . $Parser -> getPassHash($this -> body['password'])."'"
-            );
-//        );
+            )
+        );
     }
 
     private function logout(){
