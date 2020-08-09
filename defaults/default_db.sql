7a237d0179725a331179e95258e50410410c6aca
GermanSincovic <german.sincovic.92@gmail.com>
2020-08-09 14:58:17 +0300

    Creation of table 'access_rules'

--- defaults/default_db.sql
+++ defaults/default_db.sql
@@ -3,9 +3,9 @@ CREATE DATABASE IF NOT EXISTS `espressolight`;
 USE `espressolight`;
 
 CREATE TABLE IF NOT EXISTS `accounts` (
-    `account_id` bigint(20) NOT NULL AUTO_INCREMENT,
-    `owner_id` bigint(20) NOT NULL,
-    `account_name` varchar(250) NOT NULL DEFAULT '',
+    `account_id`    bigint(20) NOT NULL AUTO_INCREMENT,
+    `owner_id`      bigint(20) NOT NULL,
+    `account_name`  varchar(250) NOT NULL DEFAULT '',
     PRIMARY KEY (`account_id`)
 );
 
@@ -19,85 +19,100 @@ CREATE TABLE IF NOT EXISTS `branches`
 );
 
 CREATE TABLE IF NOT EXISTS `contragents` (
-    `contragent_id` bigint(20) NOT NULL AUTO_INCREMENT,
-    `account_id` bigint(20) NOT NULL,
-    `contragent_name` varchar(100) NOT NULL,
-    `contragent_comment` text NOT NULL DEFAULT '',
+    `contragent_id`         bigint(20) NOT NULL AUTO_INCREMENT,
+    `account_id`            bigint(20) NOT NULL,
+    `contragent_name`       varchar(100) NOT NULL,
+    `contragent_comment`    text NOT NULL DEFAULT '',
     PRIMARY KEY (`contragent_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `expenses` (
-    `expense_id` bigint(20) NOT NULL AUTO_INCREMENT,
-    `nomenclature_id` bigint(20) NOT NULL,
-    `branch_id` bigint(20) NOT NULL,
-    `user_id` bigint(20) NOT NULL,
-    `expense_count` int(11) NOT NULL,
-    `expense_price` float NOT NULL,
-    `expense_datetime` varchar(50) NOT NULL,
+    `expense_id`        bigint(20) NOT NULL AUTO_INCREMENT,
+    `nomenclature_id`   bigint(20) NOT NULL,
+    `branch_id`         bigint(20) NOT NULL,
+    `user_id`           bigint(20) NOT NULL,
+    `expense_count`     int(11) NOT NULL,
+    `expense_price`     float NOT NULL,
+    `expense_datetime`  varchar(50) NOT NULL,
     PRIMARY KEY (`expense_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `incoming` (
-    `incoming_id` bigint(20) NOT NULL AUTO_INCREMENT,
-    `nomenclature_id` bigint(20) NOT NULL,
-    `branch_id` bigint(20) NOT NULL,
-    `user_id` bigint(20) NOT NULL,
-    `incoming_count` int(11) NOT NULL,
-    `incoming_price` float NOT NULL,
-    `incoming_datetime` varchar(50) NOT NULL,
+    `incoming_id`           bigint(20) NOT NULL AUTO_INCREMENT,
+    `nomenclature_id`       bigint(20) NOT NULL,
+    `branch_id`             bigint(20) NOT NULL,
+    `user_id`               bigint(20) NOT NULL,
+    `incoming_count`        int(11) NOT NULL,
+    `incoming_price`        float NOT NULL,
+    `incoming_datetime`     varchar(50) NOT NULL,
     PRIMARY KEY (`incoming_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `nomenclature` (
-    `nomenclature_id` bigint(20) NOT NULL AUTO_INCREMENT,
-    `account_id` bigint(20) NOT NULL,
-    `nomenclature_name` varchar(100) NOT NULL,
-    `nomenclature_measurement` varchar(50) NOT NULL,
-    `nomenclature_excise` tinyint(4) NOT NULL,
+    `nomenclature_id`           bigint(20) NOT NULL AUTO_INCREMENT,
+    `account_id`                bigint(20) NOT NULL,
+    `nomenclature_name`         varchar(100) NOT NULL,
+    `nomenclature_measurement`  varchar(50) NOT NULL,
+    `nomenclature_excise`       tinyint(4) NOT NULL,
     PRIMARY KEY (`nomenclature_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `prices` (
-    `price_id` bigint(20) NOT NULL AUTO_INCREMENT,
-    `account_id` bigint(20) NOT NULL,
-    `nomenclature_id` bigint(20) NOT NULL,
-    `price_amount` float NOT NULL,
+    `price_id`              bigint(20) NOT NULL AUTO_INCREMENT,
+    `account_id`            bigint(20) NOT NULL,
+    `nomenclature_id`       bigint(20) NOT NULL,
+    `price_amount`          float NOT NULL,
     PRIMARY KEY (`price_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `recipes` (
-    `recipe_id` bigint(20) NOT NULL AUTO_INCREMENT,
-    `account_id` bigint(20) NOT NULL,
-    `recipe_name` varchar(100) NOT NULL,
-    `recipe_ingredients` tinytext NOT NULL,
+    `recipe_id`             bigint(20) NOT NULL AUTO_INCREMENT,
+    `account_id`            bigint(20) NOT NULL,
+    `recipe_name`           varchar(100) NOT NULL,
+    `recipe_ingredients`    text NOT NULL,
     PRIMARY KEY (`recipe_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `roles` (
-    `role_id` bigint(20) NOT NULL AUTO_INCREMENT,
-    `account_id` bigint(20) NOT NULL,
-    `role_name` varchar(250) NOT NULL DEFAULT '',
-    `role_access_users` varchar(5) DEFAULT '--',
-    `role_access_accounts` varchar(5) DEFAULT '--',
-    `role_access_branches` varchar(5) DEFAULT '--',
+    `role_id`               bigint(20) NOT NULL AUTO_INCREMENT,
+    `account_id`            bigint(20) NOT NULL,
+    `role_name`             varchar(250) NOT NULL DEFAULT '',
+    `role_access_users`     varchar(5) DEFAULT '--',
+    `role_access_accounts`  varchar(5) DEFAULT '--',
+    `role_access_branches`  varchar(5) DEFAULT '--',
     PRIMARY KEY (`role_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `users` (
-    `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
-    `account_id` bigint(20) NOT NULL,
-    `role_id` bigint(20) NOT NULL,
-    `branch_id` bigint(20) DEFAULT NULL,
-    `user_login` varchar(100) NOT NULL,
-    `user_password` varchar(250) NOT NULL,
-    `user_first_name` varchar(250) NOT NULL,
-    `user_second_name` varchar(250) NOT NULL,
-    `user_full_name` varchar(250) NOT NULL,
-    `user_email` varchar(250) NOT NULL,
-    `user_phone` varchar(250) NOT NULL,
-    `user_comment` text NOT NULL DEFAULT '',
-    `user_active` tinyint(4) NOT NULL DEFAULT 0,
+    `user_id`               bigint(20) NOT NULL AUTO_INCREMENT,
+    `account_id`            bigint(20) NOT NULL,
+    `role_id`               bigint(20) NOT NULL,
+    `branch_id`             bigint(20) DEFAULT NULL,
+    `user_login`            varchar(100) NOT NULL,
+    `user_password`         varchar(250) NOT NULL,
+    `user_first_name`       varchar(250) NOT NULL,
+    `user_second_name`      varchar(250) NOT NULL,
+    `user_full_name`        varchar(250) NOT NULL,
+    `user_email`            varchar(250) NOT NULL,
+    `user_phone`            varchar(250) NOT NULL,
+    `user_comment`          text NOT NULL DEFAULT '',
+    `user_active`           tinyint(4) NOT NULL DEFAULT 0,
     PRIMARY KEY (`user_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `users` (
+    `access_rul_id`            bigint AUTO_INCREMENT,
+    'access_rule_accounts'     varchar(50) DEFAULT '--' not null,
+    'access_rule_companies'    varchar(50) DEFAULT '--' not null,
+    'access_rule_contragents'  varchar(50) DEFAULT '--' not null,
+    'access_rule_expenses'     varchar(50) DEFAULT '--' not null,
+    'access_rule_incoming'     varchar(50) DEFAULT '--' not null,
+    'access_rule_nomenclature' varchar(50) DEFAULT '--' not null,
+    'access_rule_prices'       varchar(50) DEFAULT '--' not null,
+    'access_rule_recipes'      varchar(50) DEFAULT '--' not null,
+    'access_rule_roles'        varchar(50) DEFAULT '--' not null,
+    'access_rule_users'        varchar(50) DEFAULT '--' not null,
+    PRIMARY KEY (`access_rul_id`)
 );
 
 INSERT INTO `users` (`user_id`, `account_id`, `role_id`, `branch_id`, `user_login`, `user_password`, `user_first_name`, `user_second_name`, `user_full_name`, `user_email`, `user_phone`, `user_comment`, `user_active`) VALUES
--- index.php
+++ index.php
@@ -8,19 +8,19 @@ new MODEL_LOADER();
 
 $Router = new Router();
 
-if( $Router -> isAPI() ) {
+//if( $Router -> isAPI() ) {
     $Router -> callEndpoint();
-} else {
-    if( API_Auth::isLoggedIn() ){
-        $Router -> showPage('main');
-    } else {
-        if($Router -> urlPattern == '/login'){
-            $Router -> showPage('login');
-        } elseif ($Router -> urlPattern == '/logout'){
-            API_Auth::logout();
-            header("Location: ".DOMAIN.'/login');
-        } else {
-            $Router -> redirect('login');
-        }
-    }
-}
+//} else {
+//    if( API_Auth::isLoggedIn() ){
+//        $Router -> showPage('main');
+//    } else {
+//        if($Router -> urlPattern == '/login'){
+//            $Router -> showPage('login');
+//        } elseif ($Router -> urlPattern == '/logout'){
+//            API_Auth::logout();
+//            header("Location: ".DOMAIN.'/login');
+//        } else {
+//            $Router -> redirect('login');
+//        }
+//    }
+//}
